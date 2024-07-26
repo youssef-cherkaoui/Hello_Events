8139262ee9515ac7a99ca5f6f2eef01a63144795
TAHA SELLAK <sellaktaha1@gmail.com>
2024-07-26 11:11:29 +0100

    Modifier Repository en DTO

--- src/main/java/com/example/hello_events/Repository/ContactRepository.java
+++ src/main/java/com/example/hello_events/Repository/ContactRepository.java
@@ -1,11 +1,11 @@
 package com.example.hello_events.Repository;
 
+import com.example.hello_events.Model.Contact;
 import com.example.hello_events.Model.Dto.ContactDTO;
-import com.example.hello_events.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface ContactRepository extends JpaRepository<ContactDTO, Long> {
+public interface ContactRepository extends JpaRepository<Contact, Long> {
 }
--- src/main/java/com/example/hello_events/Repository/EventRepository.java
+++ src/main/java/com/example/hello_events/Repository/EventRepository.java
@@ -3,6 +3,8 @@ package com.example.hello_events.Repository;
 import com.example.hello_events.Model.Dto.EventDTO;
 import com.example.hello_events.Model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface EventRepository extends JpaRepository<EventDTO, Long> {
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
 }
